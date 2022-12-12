cd1d408e21de4e1ef93327186b41b957dd91232b
toandh <toandh.thuylinh@gmail.com>
2022-12-12 08:58:44 +0700

    api gateway

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare module 'express' {
+  export interface Request {
+    user: any;
+  }
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
